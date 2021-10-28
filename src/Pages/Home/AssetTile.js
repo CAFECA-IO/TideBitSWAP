af3d92864b3b10c15b6fefdfa66a24174fca42de
TzuHanLiang <crocodilestear@gmail.com>
2021-10-28 14:00:40 +0800

    feat: add decimal formate

--- src/Pages/Home/AssetTile.js
+++ src/Pages/Home/AssetTile.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import UserContext from "../../store/user-context";
+import { formateDecimal } from "../../Utils/utils";
 import classes from "./AssetTile.module.css";
 
 const AssetTile = (props) => {
@@ -11,9 +12,9 @@ const AssetTile = (props) => {
       </div>
       <div className={classes.name}>{props.symbol}</div>
       <div className={classes.composition}>
-        <span>{props.composition[0]} + </span>
+        <span>{formateDecimal(props.composition[0], 12, 2)} + </span>
         <div className="tooltip">
-          <div>{props.composition[1]}</div>
+          <div>{formateDecimal(props.composition[1], 12, 2)}</div>
           <div className={`tooltiptext ${classes.tooltiptext}`}>Locked</div>
         </div>
       </div>
