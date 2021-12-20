c3a33e097e9447a13f11e7938d6ce96bba304971
Luphia Chang <luphia@cafeca.io>
2021-12-20 20:37:11 +0800

    Update contract address

--- backend/constants/TideBitSwapData.js
+++ backend/constants/TideBitSwapData.js
@@ -2,5 +2,9 @@ module.exports = [
   {
     router: `0xDDBCB302A16f27D12Ef1cA491b4791a7b3d67c04`,
     chainId: 3,
+  },
+  {
+    router: `0xB1d331b30e2DE2202136e9d9131Dc5ddFfbE8E28`,
+    chainId: 1,
   }
 ]
