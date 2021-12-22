cfe72c0248771ab777aa3763402dbaf5856874c8
TzuHanLiang <crocodilestear@gmail.com>
2021-12-22 18:42:56 +0800

    feat: update summary

--- src/screens/Swap/Swap.js
+++ src/screens/Swap/Swap.js
@@ -37,7 +37,7 @@ const Swap = (props) => {
   // const [openExpertMode, setOpenExpertMode] = useState(false);
 
   const getDetails = useCallback(
-    async (pool, active, passive, slippage) => {
+    async (pool, active, passive, slippage, lastAmountChangeType) => {
       console.log(
         `getDetails !!details?.length`,
         !!details?.length,
@@ -130,22 +130,44 @@ const Swap = (props) => {
             "The ultimate price and output is determined by the amount of tokens in the pool at the time of your swap.",
         },
         {
-          title: "Minimun Received",
-          value: `${
-            passive?.amount
-              ? formateDecimal(
-                  SafeMath.mult(
-                    passive?.amount,
-                    SafeMath.minus(
-                      "1",
-                      SafeMath.div(slippage?.value || "0.5", "100")
-                    )
-                  ),
-                  18
-                )
-              : "--"
-          } ${passive?.symbol || "--"}`,
-          explain: "Minimun Received output amount",
+          title:
+            lastAmountChangeType === "paired"
+              ? "Maximum sent"
+              : "Minimun Received",
+          value:
+            lastAmountChangeType === "paired"
+              ? `${
+                  passive?.amount
+                    ? formateDecimal(
+                        SafeMath.mult(
+                          passive?.amount,
+                          SafeMath.plus(
+                            "1",
+                            SafeMath.div(slippage?.value || "0.5", "100")
+                          )
+                        ),
+                        18
+                      )
+                    : "--"
+                }`
+              : `${
+                  passive?.amount
+                    ? formateDecimal(
+                        SafeMath.mult(
+                          passive?.amount,
+                          SafeMath.minus(
+                            "1",
+                            SafeMath.div(slippage?.value || "0.5", "100")
+                          )
+                        ),
+                        18
+                      )
+                    : "--"
+                } ${passive?.symbol || "--"}`,
+          explain:
+            lastAmountChangeType === "paired"
+              ? `Input is estimated. You will sell at most maximum amount or the transaction will revert.`
+              : `Output is estimated. You will receive at least minumun amount or the transaction will revert.`,
         },
       ];
     },
@@ -244,13 +266,22 @@ const Swap = (props) => {
           amount: updateSelectedAmount,
         },
         { ..._passive, amount: updatePairedAmount },
-        slippage
+        slippage,
+        lastAmountChangeType
       );
       console.log(`getDetails details`, details);
       setDetails(details);
       setIsLoading(false);
     },
-    [connectorCtx, getDetails, pairedCoin, selectedCoin, selectedPool, slippage]
+    [
+      connectorCtx,
+      getDetails,
+      pairedCoin,
+      selectedCoin,
+      selectedPool,
+      slippage,
+      lastAmountChangeType,
+    ]
   );
 
   const tokenExchangerHander = async () => {
@@ -347,7 +378,13 @@ const Swap = (props) => {
         pool,
       });
       if (pool) {
-        const details = await getDetails(pool, _active, _passive, slippage);
+        const details = await getDetails(
+          pool,
+          _active,
+          _passive,
+          slippage,
+          lastAmountChangeType
+        );
         console.log(`getDetails details`, details);
         setDetails(details);
         const histories = await connectorCtx.getPoolHistory(pool.poolContract);
@@ -367,6 +404,7 @@ const Swap = (props) => {
       selectedCoin,
       selectedCoinAmount,
       slippage,
+      lastAmountChangeType,
     ]
   );
 
@@ -523,6 +561,7 @@ const Swap = (props) => {
         message: `${
           SafeMath.gt(value, 1) ? "Your transaction may be frontrun" : ""
         }`,
+        lastAmountChangeType,
       }
     );
     setDetails(details);
@@ -543,7 +582,8 @@ const Swap = (props) => {
       {
         value: "0.1",
         message: "",
-      }
+      },
+      lastAmountChangeType
     );
     setDetails(details);
   };
